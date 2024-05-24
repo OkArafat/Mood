848fb50f78ba8d54bfd9e13953e524c33230d841
Arafat Haq Biswas <121508342+OkArafat@users.noreply.github.com>
2024-05-24 19:44:59 +0600

    Add files via upload

---
+++ static/media/background.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Background Audio</title>
+</head>
+<body>
+    <audio class="audio" autoplay>
+        <source src="static/media/background.mp3" type="audio/mp3">
+    </audio>
+    
+</body>
+</html>
